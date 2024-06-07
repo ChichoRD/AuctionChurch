1eea4614fee1f0826a42ae5739575f05c5f8b3ec
ChichoRD <chichord2005@hotmail.com>
2024-06-07 20:28:06 +0200

    feat: Added event to track specific variations

--- Assets/Systems/Status/Effector/StatusVariation.cs
+++ Assets/Systems/Status/Effector/StatusVariation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace StatusSystem.Effector
 {
@@ -9,20 +10,26 @@ namespace StatusSystem.Effector
         private float _defaultVariation = 0.0f;
         public float Variation { get; set; }
 
+        [field: SerializeField]
+        public UnityEvent<float> VariationApplied { get; private set; }
+
         private void OnEnable()
         {
+            VariationApplied ??= new UnityEvent<float>();
             Variation = _defaultVariation;
         }
 
         public float Vary(ClampedStatus status)
         {
             status.Value += Variation;
+            VariationApplied.Invoke(status.Value);
             return status.Value;
         }
 
         public float Vary(Status status)
         {
             status.Value += Variation;
+            VariationApplied.Invoke(status.Value);
             return status.Value;
         }
     }
