811d202e0877b1d8bf806ac1e24878ead07e2017
reagancheatham <reagancheatham@gmail.com>
2024-06-08 13:28:27 -0500

    separating commits and cleaning up my file structure

--- Assets/Systems/Interaction/Player Interaction/PlayerInteractor.cs
+++ Assets/Systems/Interaction/Player Interaction/PlayerInteractor.cs
@@ -1,21 +1,18 @@
-using AuctionChurch.Interaction.Holding;
 using SH.AreaDetection;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace AuctionChurch.Interaction
 {
-    public class PlayerInteractor : Interactor, IInteractor<HoldableObject>
+    public class PlayerInteractor : Interactor
     {
         [SerializeField] private InputActionReference _interactionInput;
         private InteractionDetector _detector;
-        private ItemHolder _itemHolder;
 
         protected override void Awake()
         {
             base.Awake();
             _detector = GetComponentInChildren<InteractionDetector>();
-            _itemHolder = GetComponentInChildren<ItemHolder>();
         }
 
         private void OnEnable()
@@ -39,14 +36,6 @@ namespace AuctionChurch.Interaction
 
             for (int i = 0; i < interactables.Length; i++)
                 Interact(interactables[i]);
-        }
-
-        void IInteractor<HoldableObject>.Interact(HoldableObject interactable)
-        {
-            if (_itemHolder.IsHolding)
-                return;
-
-            _itemHolder.Hold(interactable);
         }
     }
 }
