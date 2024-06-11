a3710a61c188bf4cb798cc289ae05fcff952b01d
ChichoRD <chichord2005@hotmail.com>
2024-06-11 21:11:50 +0200

    feat!: Imported Dialogue System

---
+++ Assets/Systems/Context-Aware-Dialogue-System/Rule Handler/IDialogueRuleHandler.cs
@@ -0,0 +1,25 @@
+﻿using ContextualDialogueSystem.Event;
+using ContextualDialogueSystem.Rule;
+using ContextualDialogueSystem.Rule.Criteria;
+using System.Threading.Tasks;
+
+namespace ContextualDialogueSystem.RuleHandler
+{
+    public interface IDialogueRuleHandler
+    {
+        Task<bool> HandleRule<TRuleContent>(IDialogueRule<TRuleContent, ICriteria> dialogueRule);
+    }
+
+    public interface IDialogueRuleHandler<in TRuleContent> : IDialogueRuleHandler, ISubscribableDialogueRuleHandler
+    {
+        bool ISubscribableDialogueRuleHandler.TrySubscribeToDialogueEvent(IObservableDialogueEvent dialogueEvent) =>
+            dialogueEvent != null && dialogueEvent.Subscribe(this);
+
+        bool ISubscribableDialogueRuleHandler.TryUnsubscribeFromDialogueEvent(IObservableDialogueEvent dialogueEvent) =>
+            dialogueEvent != null && dialogueEvent.Unsubscribe(this);
+        Task<bool> IDialogueRuleHandler.HandleRule<URuleContent>(IDialogueRule<URuleContent, ICriteria> dialogueRule) =>
+            dialogueRule is IDialogueRule<TRuleContent, ICriteria> rule ? HandleRule(rule) : Task.FromResult(false);
+
+        Task<bool> HandleRule(IDialogueRule<TRuleContent, ICriteria> dialogueRule);
+    }
+}
